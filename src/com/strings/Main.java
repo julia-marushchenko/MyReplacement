0b7a3f84f4b9fca8dccbcc089e423e95ab477b77
Julia Marushchenko <marushchenko.yulia@gmail.com>
2025-08-09 10:57:29 +0200

    Add files via upload
    Committing Main class

---
+++ src/com/strings/Main.java
@@ -0,0 +1,18 @@
+// Java program to convert replace letter 'l' with 's'
+
+package com.strings;
+
+// Main class
+public class Main {
+
+    // Main method
+    public static void main(String[] args) {
+
+        // Creating string
+        String str = new String("A string with abcdefghijklmnopqrstuvwxyz.");
+
+        // Converting str to upper case and printing to console
+        System.out.println(str.replace('l', 's')); // Output: A string with abcdefghijksmnopqrstuvwxyz.
+
+    }
+}
